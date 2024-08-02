ce5ca86e83ddfff11f7c7f09102db973dca91867
sylvain kadjo <sylvainka12@gmail.com>
2024-08-02 13:47:15 +0400

    add lighter color to tailwindcss global variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-const plugin = require('tailwindcss');
-const {fontFamily} = require("tailwindcss/defaultTheme")
+const plugin = require("tailwindcss");
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 
@@ -15,30 +15,31 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'fdfp': {
-          main: 'var(--fdfp-main-color)',
-          'main-light': 'var(--fdfp-main-color-light)',
-          second: 'var(--fdfp-second-color)',
-          bg: 'var(--fdfp-bg-color)',
-          bgsecond: 'var(--fdfp-bg-color-second)',
-          text: 'var(--fdfb-text-color)',
-          textsecond: 'var(--fdfb-text-second-color)',
-          light: 'var(--fdfp-light-color)'
-        }
+        fdfp: {
+          main: "var(--fdfp-main-color)",
+          "main-light": "var(--fdfp-main-color-light)",
+          second: "var(--fdfp-second-color)",
+          lighter: "var(--fdfp-bg-color-lighter)",
+          bg: "var(--fdfp-bg-color)",
+          bgsecond: "var(--fdfp-bg-color-second)",
+          text: "var(--fdfb-text-color)",
+          textsecond: "var(--fdfb-text-second-color)",
+          light: "var(--fdfp-light-color)",
+        },
       },
       screens: {
-        tablet: '820px',
+        tablet: "820px",
       },
       fontFamily: {
         manrope: ["Manrope", ...fontFamily.sans],
-        monospace: ['Major Mono Display', ...fontFamily.mono],
-        serif: ['var(--ff-serif)', ...fontFamily.serif]
+        monospace: ["Major Mono Display", ...fontFamily.mono],
+        serif: ["var(--ff-serif)", ...fontFamily.serif],
       },
     },
   },
   plugins: [
     plugin(function ({ addVariant }) {
-      addVariant('not-last-child', '&:not(:last-child)');
-    })
+      addVariant("not-last-child", "&:not(:last-child)");
+    }),
   ],
 };
