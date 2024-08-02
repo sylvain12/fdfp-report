b8d33a7dbeea8b89d43fa58de1fd50774ebbc62a
sylvain kadjo <sylvainka12@gmail.com>
2024-08-02 14:06:32 +0400

    add report total view hook

--- src/store/report.store.ts
+++ src/store/report.store.ts
@@ -1,11 +1,11 @@
-import axios from 'axios';
-import { stringify } from 'querystring';
+import axios from "axios";
+import { stringify } from "querystring";
 import { create } from "zustand";
 
 export interface TTableFilterStoreStore {
-    entity: string,
-    entitylabel: string,
-    procname: string,
+  entity: string;
+  entitylabel: string;
+  procname: string;
 }
 
 type TableFilterStore = {
@@ -16,11 +16,26 @@ type TableFilterStore = {
 export const useTableFilterStore = create<TableFilterStore>((set) => ({
   tables: [],
   setTables: async (content: TTableFilterStoreStore[]) => {
-    set({tables: content})
-  }
+    set({ tables: content });
+  },
 }));
-
 
 export const useReportStore = () => {
   return {};
 };
+
+type ReportTotalStore = {
+  isVisible: boolean;
+  setVisibility: (visibility: boolean) => void;
+  resetVisibility: () => void;
+};
+
+export const useReportTotalStore = create<ReportTotalStore>((set) => ({
+  isVisible: false,
+  setVisibility: (visibility: boolean) => {
+    set((state) => ({ isVisible: visibility }));
+  },
+  resetVisibility: () => {
+    set((state) => ({ isVisible: false }));
+  },
+}));
