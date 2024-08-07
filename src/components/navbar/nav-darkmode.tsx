400505b8a4686781b279e7574b8cdf72f2b0d969
sylvain kadjo <sylvainka12@gmail.com>
2024-08-07 22:17:26 +0400

    add dark mode functionality

--- src/components/navbar/nav-darkmode.tsx
+++ src/components/navbar/nav-darkmode.tsx
@@ -2,9 +2,25 @@
 
 import { useDarkModeStore } from "@/store/navs.store";
 import { SunIcon, MoonIcon } from "@heroicons/react/24/outline";
+import { useEffect } from "react";
 
 export default function NavDarkMode() {
   const { isDark, toggleMode } = useDarkModeStore();
+
+  useEffect(() => {
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+    // const selectedTheme = localStorage.getItem("theme");
+
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [isDark]);
 
   return (
     <div
