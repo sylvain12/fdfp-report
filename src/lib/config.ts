d1d0a3deed51fca7b8ee47f0ace8ce251988d10d
sylvain kadjo <sylvainka12@gmail.com>
2024-10-08 03:17:45 +0400

    add api path

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,3 +4,5 @@ export const API_URL =
 // export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://147.182.139.206:8016/api"
 export const API_SUBGROUP_PATH = "reports/parameter.subgroup.values";
 export const API_STATISTICS_PATH = "reports/statistic";
+export const API_DASHBOARD_AGREED_PRODUCT_PATH = 'dashboard/agreed-products'
+export const API_DASHBOARD_TRAINING_PLAN_PATH = "dashboard/training-plan";
