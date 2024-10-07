357f7e409216c8ba0d6b32dc61e440d0a0c8e9da
sylvain kadjo <sylvainka12@gmail.com>
2024-10-08 03:20:22 +0400

    uncomment ErrorBoundary and Suspense Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,10 @@ import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import Fallback from "@/components/errors/fallback";
 import dynamic from "next/dynamic";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Dashboard from "@/components/dashbaord/dashboard";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const SuspenseDashboardComponent = dynamic(
   () => import("@/components/dashbaord/dashboard"),
@@ -23,14 +21,15 @@ const SuspenseDashboardComponent = dynamic(
 
 export default function Home() {
   return (
-    <div className="tablet:px-[10rem] tablet:py-[6.5rem] px-[2rem] py-[3rem]">
-          {/* <ErrorBoundary fallback={<Fallback />}>
-            <Suspense fallback={<Loader />}> */}
-                 {/* <QueryClientProvider client={queryClient}> */}
-              {/* <SuspenseDashboardComponent /> */}
-                  {/* </QueryClientProvider> */}
-            {/* </Suspense>
-          </ErrorBoundary> */}
+    <div className="px-[20rem] py-[3rem]">
+      {/* <Dashboard /> */}
+      <ErrorBoundary fallback={<Fallback />}>
+        <Suspense fallback={<Loader />}>
+          <QueryClientProvider client={queryClient}>
+            <SuspenseDashboardComponent />
+          </QueryClientProvider>
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
