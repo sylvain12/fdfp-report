89e4d3544ceceab4fb845118f2f37ffc0fdd8f2c
sylvain kadjo <sylvainka12@gmail.com>
2024-10-08 03:25:34 +0400

    Report details layout
    - remove react query provider
    - totals display condition

--- src/app/reports/[slug]/details/layout.tsx
+++ src/app/reports/[slug]/details/layout.tsx
@@ -21,7 +21,7 @@ import clsx from "clsx";
 import { useGetData } from "@/store/table-data.store";
 import ReportTableTotals from "@/components/reports/details/report-table-totals";
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 const defaultItem = [
   {
@@ -78,7 +78,7 @@ const ReportDetailsLayout = () => {
   }, []);
 
   return (
-    <QueryClientProvider client={queryClient}>
+    // <QueryClientProvider client={queryClient}>
       <div className="report-details-container">
         {/* <ErrorBoundary fallback={<Fallback />}>
           <Suspense fallback={<Loader />}> */}
@@ -90,7 +90,10 @@ const ReportDetailsLayout = () => {
             onClick={() => setVisibility(true)}
             className={clsx(
               "flex justify-center items-center uppercase gap-4 text-fdfp-second underline text-[1.25rem] font-medium cursor-pointer",
-              { "opacity-55 pointer-events-none": data === null || isVisible }
+              {
+                "opacity-55 pointer-events-none":
+                  data === null || data.totals === undefined || isVisible,
+              }
             )}
           >
             <Icon icon="typcn:arrow-back-outline" width="26" />
@@ -102,7 +105,7 @@ const ReportDetailsLayout = () => {
         {/* </Suspense>
         </ErrorBoundary> */}
       </div>
-    </QueryClientProvider>
+    // </QueryClientProvider>
   );
 };
 
