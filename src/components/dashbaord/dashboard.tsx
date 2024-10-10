c5f1354fccd9f677e4b49d3994b32e8e86b1cca4
sylvain kadjo <sylvainka12@gmail.com>
2024-10-11 02:49:03 +0400

    remove unused component

--- src/components/dashbaord/dashboard.tsx
+++ src/components/dashbaord/dashboard.tsx
@@ -1,7 +1,7 @@
 import DashboardHeader from "./dashboard-header";
 import DashboardPart1 from './dashboard-part-1';
 import DashboardPart2 from './dashboard-part-2';
-import DashboardPart3 from './dashboard-part-3'
+
 import React from 'react';
 
 export default function Dashboard() {
