d10fa8a484a88c35d5f72a21f6731c0acf1a0601
sylvain kadjo <sylvainka12@gmail.com>
2024-11-05 23:27:35 +0400

    replace report totals component by shadcn ui popover

--- src/app/reports/[slug]/details/layout.tsx
+++ src/app/reports/[slug]/details/layout.tsx
@@ -9,8 +9,8 @@ import {
 } from "@/components/breadcrumb/breadcrumb.model";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { stringify } from "querystring";
-import { Suspense, useEffect, useState } from "react";
-import { getReportPathDetails } from "@/lib/utils";
+import { Suspense, useEffect, useMemo, useState } from "react";
+import { currencyFormatter, getReportPathDetails } from "@/lib/utils";
 import Loader from "@/components/animation/loader";
 import { ErrorBoundary } from "react-error-boundary";
 import Fallback from "@/components/errors/fallback";
@@ -20,6 +20,14 @@ import { useReportTotalStore } from "@/store/report.store";
 import clsx from "clsx";
 import { useGetData } from "@/store/table-data.store";
 import ReportTableTotals from "@/components/reports/details/report-table-totals";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { ReportTotalLoader } from '@/components/reports/details/report-loader';
+import { filterFormValue } from "@/components/reports/details/report-table-filters-form";
+
 
 // const queryClient = new QueryClient();
 
@@ -38,9 +46,9 @@ const ReportDetailsLayout = () => {
     items: defaultItem,
   });
 
-  const { data } = useGetData();
-
-  const { setVisibility, isVisible } = useReportTotalStore();
+  const data = useGetData(state => state.data);
+	const loading = useGetData(state => state.loading)
+	const isVisible = useReportTotalStore(state => state.isVisible)
 
   const searchParams = useSearchParams();
   const pathname = usePathname();
@@ -48,7 +56,8 @@ const ReportDetailsLayout = () => {
   const pathDetails = getReportPathDetails(pathname);
 
   const currentPageName = searchParams.get("name");
-  const year = searchParams.get("year");
+  // const year = searchParams.get("year");
+	const { year } = filterFormValue.value;
 
   const parentItem: TBreadcrumbItem = {
     title: pathDetails.name,
@@ -69,25 +78,31 @@ const ReportDetailsLayout = () => {
     isCurrent: true,
   };
 
+	const totalsData = useMemo(() => {
+		if (loading || data === null) return null;
+		return data !== null ? data.totals : null;
+	}, [year, data]);
+
   useEffect(() => {
     setBreadcrumb((b) => ({
       ...breadcrumb,
       items: [...breadcrumb.items, parentItem, currentItem],
     }));
+
     return () => {};
   }, []);
 
   return (
     // <QueryClientProvider client={queryClient}>
-      <div className="report-details-container">
-        {/* <ErrorBoundary fallback={<Fallback />}>
+    <div className="report-details-container">
+      {/* <ErrorBoundary fallback={<Fallback />}>
           <Suspense fallback={<Loader />}> */}
-        <ReportTableTotals />
-        <div className="flex justify-between items-center max-md:flex-col max-md:items-end">
-          <Breadcrumb {...breadcrumb} />
+      {/* <ReportTableTotals /> */}
+      <div className="flex justify-between items-center max-md:flex-col max-md:items-end">
+        <Breadcrumb {...breadcrumb} />
 
-          <div
-            onClick={() => setVisibility(true)}
+        <Popover>
+          <PopoverTrigger
             className={clsx(
               "flex justify-center items-center uppercase gap-4 text-fdfp-second underline text-[1.25rem] font-medium cursor-pointer",
               {
@@ -98,13 +113,56 @@ const ReportDetailsLayout = () => {
           >
             <Icon icon="typcn:arrow-back-outline" width="26" />
             Afficher les totaux
-          </div>
-        </div>
+          </PopoverTrigger>
+          <PopoverContent className={`${totalsData && totalsData.length > 2 ? 'w-[700px]' : 'w-[650px]'}`} align='end'>
+            <div className="p-4">
+              <div className=" mb-6">
+                <p className="text-[1.3rem]">Annee</p>
+                <span className="text-fdfp-second text-[1.9em] font-space-grotesk">
+                  {year}
+                </span>
+              </div>
+           {/* {isVisible &&   */}
+              <div className={clsx("grid grid-flow-row auto-rows-max items-end gap-[4rem] gap-y-[2rem]", {
+'grid-cols-3': totalsData && totalsData.length > 2,	
+'grid-cols-2': totalsData && totalsData.length <= 2})}>
+                {totalsData &&
+                  totalsData!.map((total) => (
+                    <div
+                      key={total.label}
+                      className=""
+                    >
+                      <p className='text-[1.4rem] font-clash-display'>{total?.label!}</p>
+                      <span className="font-space-grotesk text-fdfp-second font-medium text-[1.95rem]">
+                        {currencyFormatter(total.value, " ")}
+                      </span>
+                    </div>
+                  ))}
+              </div>
+            {/* } */}
+            </div>
+          </PopoverContent>
+        </Popover>
 
-        <ReportTableDetails />
-        {/* </Suspense>
-        </ErrorBoundary> */}
+        {/* <div
+          onClick={() => setVisibility(true)}
+          className={clsx(
+            "flex justify-center items-center uppercase gap-4 text-fdfp-second underline text-[1.25rem] font-medium cursor-pointer",
+            {
+              "opacity-55 pointer-events-none":
+                data === null || data.totals === undefined || isVisible,
+            }
+          )}
+        >
+          <Icon icon="typcn:arrow-back-outline" width="26" />
+          Afficher les totaux
+        </div> */}
       </div>
+
+      <ReportTableDetails />
+      {/* </Suspense>
+        </ErrorBoundary> */}
+    </div>
     // </QueryClientProvider>
   );
 };
